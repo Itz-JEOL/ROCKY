9a3ea7a5acad8ff9f711e2ee25086421f801d829
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-02-26 18:13:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config(object):
     SPAMWATCH_API = None
     TOKEN = getenv("TOKEN", None)
     OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "jeol_tg")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "beta_support")
     LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
     MONGO_URI = getenv("MONGO_DB_URI")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
