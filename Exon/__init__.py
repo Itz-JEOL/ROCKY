c6b007141b642101592f4ae6e1d5f92a20801b5b
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-02-26 15:09:43 +0530

    Update __init__.py

--- Exon/__init__.py
+++ Exon/__init__.py
@@ -186,6 +186,34 @@ arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 loop = asyncio.get_event_loop()
 
 
+# ᴇᴠᴇɴᴛs
+def register(**args):
+    """ʀᴇɢɪsᴛᴇʀs ᴀ ɴᴇᴡ ᴍᴇssᴀɢᴇ."""
+    pattern = args.get("pattern")
+
+    r_pattern = r"^[/!]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.sender_id and check.sender_id != OWNER_ID:
+                pass
+            try:
+                await func(check)
+            except BaseException:
+                return
+            else:
+                pass
+
+        tbot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
 async def get_entity(client, entity):
     entity_client = client
     if not isinstance(entity, Chat):
