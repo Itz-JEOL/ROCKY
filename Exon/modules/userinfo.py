2397e5fc6f5bd213cb0bac45917cdfd4d5d9e5a8
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-02-09 12:11:06 +0530

    Update userinfo.py

--- Exon/modules/userinfo.py
+++ Exon/modules/userinfo.py
@@ -344,7 +344,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/Abishnoi_bots/60">ʟᴇᴠᴇʟʟɪɴɢ</a>]'.format(
+        text += ' [<a href="https://t.me/NoxiousBgmiStore">ʟᴇᴠᴇʟʟɪɴɢ</a>]'.format(
             bot.username,
         )
 
@@ -381,11 +381,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/Abishnoi_bots/60"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/NoxiousBgmiStore"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/Abishnoi_bots/60"
-                            ),
+                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/NoxiousBgmiStore"
                         ],
                     ]
                 ),
@@ -401,10 +400,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/Abishnoi_bots/60"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/NoxiousBgmiStore"
                             ),
                             InlineKeyboardButton(
-                                "ʟᴇᴠᴇʟʟɪɴɢ", url="https://t.me/Abishnoi_bots/60"
+                                "ʟᴇᴠᴇʟʟɪɴɢ", url="https://t.me/NoxiousBgmiStore"
                             ),
                         ],
                     ]
